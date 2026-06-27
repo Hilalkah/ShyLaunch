f869dda84f9dbacc346a6926e5d588182ddd0070
Hilal Kahraman <hllkhrmn@hotmail.com>
2026-06-28 01:20:29 +0300

    Initial Commit

---
+++ ShyLaunch/ShyLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShyLaunchApp.swift
+//  ShyLaunch
+//
+//  Created by Hilal Kahraman on 28.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct ShyLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
